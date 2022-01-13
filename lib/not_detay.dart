87f17930ace5a2dd41645e5fa636b8e6d8491390
Mehmet Doğru <mdogru685@gmail.com>
2022-01-14 00:21:54 +0300

    Note Page edit

--- lib/not_detay.dart
+++ lib/not_detay.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_not_sepeti/models/kategori.dart';
+import 'package:flutter_not_sepeti/utils/database_helper.dart';
 
 class NotDetay extends StatefulWidget {
   String? baslik;
@@ -10,11 +12,71 @@ class NotDetay extends StatefulWidget {
 }
 
 class _NotDetayState extends State<NotDetay> {
+  var formKey = GlobalKey<FormState>();
+  List<Kategori>? tumKategoriler;
+  DatabaseHelper? databaseHelper;
+  int? kategoriID = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    tumKategoriler = <Kategori>[];
+    databaseHelper = DatabaseHelper();
+    databaseHelper!.kategorileriGetir().then((kategoriIcerenMapListesi) {
+      setState(() {
+        for (var okunanMap in kategoriIcerenMapListesi) {
+          tumKategoriler!.add(Kategori.fromMap(okunanMap));
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Center(child: Text(widget.baslik!))),
-      body: Container(),
+      appBar: AppBar(title: Text(widget.baslik!)),
+      body: tumKategoriler!.length <= 0
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Container(
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: [
+                        const Text("Kategori : "),
+                        Container(
+                          child: DropdownButtonHideUnderline(
+                            child: DropdownButton(
+                              items: kategoriItemleriGetir(),
+                              value: kategoriID,
+                              onChanged: (int? secilenKategoriID) {
+                                setState(() {
+                                  kategoriID = secilenKategoriID;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
     );
+  }
+
+  List<DropdownMenuItem<int>> kategoriItemleriGetir() {
+    return tumKategoriler!
+        .map(
+          (kategori) => DropdownMenuItem<int>(
+            value: kategori.kategoriID,
+            child: Text(kategori.kategoriBaslik!),
+          ),
+        )
+        .toList();
   }
 }
